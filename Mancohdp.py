f5933108825b53857fc00fe0670480a8c2e36643
Angelozel <leonelangelooropeza@gmail>
2021-08-26 19:10:24 -0500

    Again Panda

---
+++ Mancohdp.py
@@ -0,0 +1,18 @@
+print("""
+   ¡¡¡¡ BIENVENIDO A LA EMPRESA PANDIPANDA !!!!
+""")
+
+nombre = input("Ingrese su nombre: ")
+print(nombre)
+
+años = input("Cuantos años va trabajando en la empresa?: ")
+opcion = 43
+if años > opcion :
+    print("Usted tiene un bono de 5000$ ....Felicidades!!")
+elif años < opcion :
+    print("Usted no tiene ningún bono aún ")
+elif años == opcion:
+    print("Usted tiene un bono de 5000$ ....Felicidades!!")     
+   
+else:
+    print("Por favor escriba cuantos años va trabajando")
