6517ae4113e9d62bbfdeffb5aebaadad9b996b7c
Angelozel <leonelangelooropeza@gmail>
2021-08-28 17:33:49 -0500

    Ejercisio Panda

--- hello.py
+++ hello.py
@@ -1,24 +1,31 @@
-print("""
+### Ejercisio de Panda sobre una empresa ####
+def run():
+        
+    menu = """
    ¡¡¡¡ BIENVENIDO A LA EMPRESA PANDIPANDA !!!!
-""")
+"""
+    opcion = print(menu)
+    nombre = input("Ingrese su nombre: ")
+    nombre = nombre.capitalize()
 
-nombre = input("Ingrese su nombre: ")
 
+    años = int(input("Cuantos años va trabajando en la empresa?: "))
 
-años = int(input("Cuantos años va trabajando en la empresa?: "))
+    opcion = 30
 
-opcion = 43
-
-if años > opcion :
-    print("Usted tiene un bono de 5000$ ....Felicidades!!")
-elif años < opcion :
-    print("Usted no tiene ningún bono aún ")
-elif años == opcion:
-    print("Usted tiene un bono de 5000$ ....Felicidades!!")     
+    if años > opcion :
+        print("Usted tiene un bono de 5000$ ....Felicidades!!")
+    elif años < opcion :
+        print("Usted no tiene ningún bono aún ")
+    elif años == opcion:
+        print("Usted tiene un bono de 5000$ ....Felicidades!!")     
    
-else:
-    print("Por favor escriba cuantos años va trabajando")
+    else:
+        print("Por favor escriba cuantos años va trabajando")
 
-print("¡¡¡ ESO ES TODO ESTIMADO Francesco Oropeza Llallihuaman")
+    print("¡¡¡ESO ES TODO ESTIMADO " + str(nombre)+"!!!") 
     
+
+if __name__== "__main__":
+    run()
       
