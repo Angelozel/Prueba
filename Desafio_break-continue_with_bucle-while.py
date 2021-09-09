a86f3e055629e4ea6a174d33bb35dbf8f7cff0df
Angelozel <leonelangelooropeza@gmail>
2021-09-09 01:10:11 -0500

    Challenge Platzi-Python-Course

---
+++ Desafio_break-continue_with_bucle-while.py
@@ -0,0 +1,32 @@
+def run():
+    # Break con el ciclo while : 
+
+    L = 1000
+    numero = 0
+    potencia_5 = 5**numero
+    while potencia_5 < L:
+        print(potencia_5)
+        numero += 1
+        potencia_5 = 5**numero
+        if potencia_5 > 100:
+            print("El numero sobrepasa 100 sorry :c ")
+            break
+
+# ---------------------------------------------------------   
+# Continue con el ciclo while :                           
+                                                          
+    L = 1000
+    numero = 0
+    potencia_5 = 5**numero
+    while potencia_5 < L:
+        print(potencia_5)
+        numero += 1
+        potencia_5 = 5**numero
+        if potencia_5 > 100:
+            continue
+
+#------------------------------------------------------------            
+
+if __name__ == "__main__":
+    run()
+    
